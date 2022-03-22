f932b61c7e2beafcfcb5ade4dce1713b78c5f742
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2022-03-22 16:59:44 +0300

    closed #6

--- Logistic.DaysOfStayTracker.Blazor/Pages/Drivers/DriverPage.razor.cs
+++ Logistic.DaysOfStayTracker.Blazor/Pages/Drivers/DriverPage.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Logistic.DaysOfStayTracker.Blazor.Components;
 using Logistic.DaysOfStayTracker.Core.Drivers;
+using Microsoft.AspNetCore.Components;
 
 namespace Logistic.DaysOfStayTracker.Blazor.Pages.Drivers;
 
@@ -8,4 +9,9 @@ public partial class DriverPage
     private DriverTable _table;
     
     private readonly DriverSearchRequest _searchRequest = new();
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = null!;
+
+    private void Create() => NavigationManager.NavigateTo("driver/");
 }
