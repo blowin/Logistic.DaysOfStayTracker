9f4dabb796243be1f2b88a7bfc07386942a6d258
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2022-03-22 16:55:03 +0300

    closed #7

--- Logistic.DaysOfStayTracker.Blazor/Pages/Countries/CountryPage.razor.cs
+++ Logistic.DaysOfStayTracker.Blazor/Pages/Countries/CountryPage.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Logistic.DaysOfStayTracker.Blazor.Components;
 using Logistic.DaysOfStayTracker.Core.Countries;
+using Microsoft.AspNetCore.Components;
 
 namespace Logistic.DaysOfStayTracker.Blazor.Pages.Countries;
 
@@ -8,4 +9,9 @@ public partial class CountryPage
     private CountryTable _table;
     
     private readonly CountrySearchRequest _searchRequest = new();
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+    
+    private void Create() => NavigationManager.NavigateTo("country/");
 }
