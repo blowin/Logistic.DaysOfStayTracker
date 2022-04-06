85346696c2e64ed83a68935cb26bd06b49bcfaf3
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2022-04-06 19:19:31 +0300

    улучшение в дизайне 1. заменил кнопки действия на кнопки с иконками 2. кнопки на добавление переименовал с '+' на нормальное 3. поменял рассположение кнопок добавления 4. изменил поиск водителя, сделал только одно поле для поиска

--- Logistic.DaysOfStayTracker.Domain/Drivers/Commands/DriverSearch.cs
+++ Logistic.DaysOfStayTracker.Domain/Drivers/Commands/DriverSearch.cs
@@ -7,8 +7,7 @@ namespace Logistic.DaysOfStayTracker.Core.Drivers.Commands;
 public class DriverSearchRequest : IRequest<IPagedList<Driver>>
 {
     public int Page { get; set; } = 1;
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+    public string? FullName { get; set; }
 }
 
 public class DriverSearchHandler : IRequestHandler<DriverSearchRequest, IPagedList<Driver>>
@@ -23,14 +22,27 @@ public class DriverSearchHandler : IRequestHandler<DriverSearchRequest, IPagedLi
     public Task<IPagedList<Driver>> Handle(DriverSearchRequest request, CancellationToken cancellationToken)
     {
         IQueryable<Driver> query = _dbContext.Drivers
-            .OrderBy(e => e.FirstName).ThenBy(e => e.LastName);
+            .OrderBy(e => e.LastName).ThenBy(e => e.FirstName);
 
-        if (!string.IsNullOrWhiteSpace(request.FirstName))
-            query = query.Where(e => e.FirstName.Contains(request.FirstName));
-
-        if (!string.IsNullOrWhiteSpace(request.LastName))
-            query = query.Where(e => e.LastName.Contains(request.LastName));
-
+        if (!string.IsNullOrWhiteSpace(request.FullName))
+        {
+            var words = request.FullName.Trim().Split(' ');
+            switch (words.Length)
+            {
+                case 0 or 1:
+                    query = query.Where(e => e.FirstName.Contains(request.FullName) || e.LastName.Contains(request.FullName));
+                    break;
+                case 2:
+                    var v1 = words[0];
+                    var v2 = words[1];
+                    query = query.Where(e => (e.FirstName.Contains(v1) && e.LastName.Contains(v2)) || (e.FirstName.Contains(v2) && e.LastName.Contains(v1)));
+                    break;
+                default:
+                    query = query.Take(0);
+                    break;
+            }
+        }
+        
         return query.ToPagedListAsync(request.Page, cancellationToken);
     }
 }
